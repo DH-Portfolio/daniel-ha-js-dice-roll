0a562773c5d4537ccd36624c8fba3d6bf9637c54
daniel-ha-pc <daniel.ha.pc97@gmail.com>
2023-08-21 21:25:10 +1200

    implemented dice roll function

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 let rollButton = document.querySelector(".rollButton");
 
 rollButton.addEventListener("click",()=>{
-    console.log("working")
+    rollDice()
 })
+
+const rollDice = roll = ()=>{
+    let randomNumber = Math.floor(Math.random()*6 + 1)
+    switch(randomNumber){
+        case 1:
+            console.log("rolled 1!")
+        break;
+        case 2:
+            console.log("rolled 2!")
+        break;
+        case 3:
+            console.log("rolled 3!")
+        break;
+        case 4:
+            console.log("rolled 4!")
+        break;
+        case 5:
+            console.log("rolled 5!")
+        break;
+        case 6:
+            console.log("rolled 6!")
+        break;
+        default:
+            console.log("rolled")
+    }
+}
